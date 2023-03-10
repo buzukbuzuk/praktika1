4deaf5d7f0771f2028a2253c083571f287c9af54
buzukbuzuk <75903448+buzukbuzuk@users.noreply.github.com>
2023-03-10 19:50:47 +0600

    Update models.py

--- studentApp/models.py
+++ studentApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default=0)
     surname = models.CharField(max_length=50, default=0)
     email = models.EmailField(max_length=50, null=True)
     phoneNum = models.CharField(max_length=50, null=True)
